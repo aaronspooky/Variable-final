c1e78ee359d240ce8a603653616236f33aff1917
Lucker <carry_things@icloud.com>
2017-03-04 16:39:45 -0600

    Primer commit

---
+++ src/paquete/Final.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int LUNA = 238857; //variables constantes final se acostumbra que sean mayusculas
+		System.out.println("Distancia a la luna " +LUNA+ "millas");
+		double lunaKilo;
+		lunaKilo = LUNA*1.609;
+		System.out.println("En kilometros es " +lunaKilo+ "km");
+	}
+
+}
